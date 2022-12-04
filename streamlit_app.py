d3f331e547ae964b89c299e80d2bb23d96313346
zdng <118778125+zdng@users.noreply.github.com>
2022-12-04 23:10:07 +0300

    Update streamlit_app.py
    Added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title('New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('menu 1')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
