077c40f9ddbe7595f0e495fa88c6d7ba169b7d9e
zdng <118778125+zdng@users.noreply.github.com>
2022-12-05 23:12:25 +0300

    Update streamlit_app.py
    connection close rows were added

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
     
 
@@ -81,6 +82,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function=insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 #streamlit.write('The user entered ', add_my_fruit)
