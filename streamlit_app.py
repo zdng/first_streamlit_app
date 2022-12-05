6789ee1eacbaf067fc67b2dd53c0eec66bbf8423
zdng <118778125+zdng@users.noreply.github.com>
2022-12-05 22:59:01 +0300

    Update streamlit_app.py
    a new button added for insert part

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,6 +69,18 @@ if streamlit.button('Get Fruit Load List'):
 #streamlit.text(my_data_row)
 #streamlit.dataframe(my_data_row)
 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+        return "Thanks for adding "+new_fruit
+    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+
+
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function=insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+#streamlit.write('The user entered ', add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
