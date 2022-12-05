b71220d3b609631f0030064202a8567f738de071
zdng <118778125+zdng@users.noreply.github.com>
2022-12-05 22:50:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ except URLError as e:
 
 #import snowflake.connector
 
-streamlit.stop()
+#streamlit.stop()
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
