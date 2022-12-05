3ed1f07db522884af37cab132133e6bb2433ce4f
zdng <118778125+zdng@users.noreply.github.com>
2022-12-05 21:59:02 +0300

    Update streamlit_app.py
    fixed 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,13 +27,13 @@ try:
 #streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response) #removed intentionally
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
