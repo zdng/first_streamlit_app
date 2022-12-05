60738f0d4964a172f3efa7b231fe086a9f0d9baf
zdng <118778125+zdng@users.noreply.github.com>
2022-12-05 21:57:55 +0300

    Update streamlit_app.py
    indentation is fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,11 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information ")
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information ")
   else:
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
